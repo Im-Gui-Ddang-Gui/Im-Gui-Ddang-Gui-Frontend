f29dc82444ecbdc6efa282d35008a13476d2afca
oajh04 <cloudtubekr@naver.com>
2021-02-04 23:35:48 +0900

    header

--- src/User/Components/main/Header.js
+++ src/User/Components/main/Header.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import * as S from '../styled/HeaderStyle';
 import Category from './Category';
+import IGDG from '../../../assets/igdg-img.png';
 
 const Header = () => {
 
     return (
         <>
+            <S.HeaderStyle>
+                <S.HeaderName>
+                    <S.Title><p>임</p>금님&nbsp;<p>귀</p>는&nbsp;<p>당</p>나귀&nbsp;<p>귀</p></S.Title>
+
+                    <S.Write>글쓰기</S.Write>
+                </S.HeaderName>
+                
+                <S.HeaderLogo>
+                    <img src={IGDG} />
+                </S.HeaderLogo>
+            </S.HeaderStyle>
             <Category />
         </>
     )

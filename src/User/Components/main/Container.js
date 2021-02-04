b97d977310c4326b5976e57e2a2bab6cbb868800
oajh04 <cloudtubekr@naver.com>
2021-02-04 23:35:45 +0900

    container

--- src/User/Components/main/Container.js
+++ src/User/Components/main/Container.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import * as S from '../styled/ContainerStyle';
 
 const Container = () => {
 
     return (
         <>
+            <S.ContainerStyle>
+                <S.ContainerHeader>
+                    <S.HeaderId>
+                        #132
+                    </S.HeaderId>
 
+                    <S.HeaderTitle>
+                        연애란 무엇인가?
+                    </S.HeaderTitle>
+
+                    <S.HeaderTag>
+                        연애
+                    </S.HeaderTag>
+
+                    <S.HeaderDate>
+                        20.11.22
+                    </S.HeaderDate>
+                </S.ContainerHeader>
+                <S.Contents>
+                    asdwasdwad
+                </S.Contents>
+            </S.ContainerStyle>
         </>
     )
 }

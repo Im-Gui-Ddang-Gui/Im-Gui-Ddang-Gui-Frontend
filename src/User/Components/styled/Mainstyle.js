bfc605f28f8b8532c358ae1dc60d1ffd29aa5bf0
oajh04 <cloudtubekr@naver.com>
2021-02-04 19:46:18 +0900

    MainStyle

---
+++ src/User/Components/styled/Mainstyle.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Mainstyle = styled.div`
+    width: 40%;
+    height: 100vh;
+    border-left: 1px solid black;
+    border-right: 1px solid black;
+    margin: 0 auto;
+`;

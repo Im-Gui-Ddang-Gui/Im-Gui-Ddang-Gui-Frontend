1487cddbb8d09324ba6325236d3e004387b76884
oajh04 <cloudtubekr@naver.com>
2021-02-04 23:36:31 +0900

    main

--- src/User/Components/styled/Mainstyle.js
+++ src/User/Components/styled/Mainstyle.js
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 
 export const Mainstyle = styled.div`
     width: 611px;
-    height: 100vh;
     border-left: 1px solid black;
     border-right: 1px solid black;
     margin: 0 auto;

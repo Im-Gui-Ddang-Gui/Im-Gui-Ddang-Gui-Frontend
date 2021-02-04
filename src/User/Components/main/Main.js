175f516d6e604ded093a00a0ec0b83e0e95e0a13
oajh04 <cloudtubekr@naver.com>
2021-02-04 21:41:22 +0900

    main

--- src/User/Components/main/Main.js
+++ src/User/Components/main/Main.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import * as S from '../styled/Mainstyle';
+import Header from './Header';
 
 const Main = () => {
 
     return (
         <>
             <S.Mainstyle>
-                ads
+                <Header />
             </S.Mainstyle>
         </>
     )

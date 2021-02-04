87cc0355ad79fed71e21d4828dfc749a158c86af
oajh04 <cloudtubekr@naver.com>
2021-02-04 21:41:18 +0900

    Header

--- src/User/Components/main/Header.js
+++ src/User/Components/main/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import * as S from '../styled/HeaderStyle';
+import Category from './Category';
 
 const Header = () => {
 
     return (
         <>
-
+            <Category />
         </>
     )
 }

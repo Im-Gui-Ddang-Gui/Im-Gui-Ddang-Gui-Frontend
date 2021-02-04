f390fb819ad0cdaeb3083c2232fcd4a6a5b9d0a2
oajh04 <cloudtubekr@naver.com>
2021-02-04 19:46:11 +0900

    Main

---
+++ src/User/Components/main/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import * as S from '../styled/Mainstyle';
+
+const Main = () => {
+
+    return (
+        <>
+            <S.Mainstyle>
+                ads
+            </S.Mainstyle>
+        </>
+    )
+}
+
+export default Main;

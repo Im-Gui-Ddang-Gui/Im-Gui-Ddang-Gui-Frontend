6286c74b0d60483b09a7ec4dbb95119dd7d289df
oajh04 <cloudtubekr@naver.com>
2021-02-05 23:52:30 +0900

    main

--- src/User/Components/main/Main.js
+++ src/User/Components/main/Main.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as S from '../styled/Mainstyle';
 import Header from './Header';
 import Body from './Body';
+import PostModal from './PostModal';
 
 const Main = () => {
+    const [ postModal, setPostModal ] = useState("visible");
+    console.log(postModal);
 
     return (
         <>
             <S.Mainstyle>
-                <Header />
+                <Header onClick={(e)=>setPostModal(e)}/>
                 <Body />
+
+                <PostModal postModal={postModal}/>
             </S.Mainstyle>
         </>
     )

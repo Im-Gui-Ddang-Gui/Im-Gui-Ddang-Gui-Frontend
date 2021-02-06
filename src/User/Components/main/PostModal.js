f146fa5f0a6f22233230485b54ea71fd88dfb307
oajh04 <cloudtubekr@naver.com>
2021-02-06 16:23:56 +0900

    postModal

--- src/User/Components/main/PostModal.js
+++ src/User/Components/main/PostModal.js
@@ -1,11 +1,33 @@
-import React from 'react';
-import 
+import React, { useState } from 'react';
+import * as S from '../styled/PostStyle';
 
-const PostModal = () => {
+const PostModal = (props) => {
+    const [ writeContents, setWriteContents] = useState("");
+
+    const onClose = () => {
+        props.setPostModal("hidden");
+        props.setHeight("0");
+    }
+
+    const onContent = (e) => {
+        setWriteContents(e.target.value);
+    }
 
     return (
         <>
-            
+            <S.PostModalStyle visibility={props.postModal}>
+                <S.PostModal height={props.height}>
+                    <div onClick={onClose}>asdawdsa</div>
+                    <S.PostContents 
+                        cols="50"
+                        rows="12"
+                        onChange={onContent}
+                        maxLength="500"
+                        required
+                    />
+                    {writeContents.length} / 500
+                </S.PostModal>
+            </S.PostModalStyle>
         </>
     )
 }

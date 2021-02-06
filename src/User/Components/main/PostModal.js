e502dd8c81c42771f2855b45749adc3441df4841
oajh04 <cloudtubekr@naver.com>
2021-02-06 18:19:23 +0900

    post

--- src/User/Components/main/PostModal.js
+++ src/User/Components/main/PostModal.js
@@ -2,11 +2,21 @@ import React, { useState } from 'react';
 import * as S from '../styled/PostStyle';
 
 const PostModal = (props) => {
+    const [ writeTitle, setWriteTitle ] = useState(null);
+    const [ writeNickName, setWriteNickName ] = useState(null);
     const [ writeContents, setWriteContents] = useState("");
 
     const onClose = () => {
         props.setPostModal("hidden");
         props.setHeight("0");
+    }
+
+    const onTitle = (e) => {
+        setWriteTitle(e.target.value);
+    }
+
+    const onNickName = (e) => {
+        setWriteNickName(e.target.value);
     }
 
     const onContent = (e) => {
@@ -20,27 +30,32 @@ const PostModal = (props) => {
                     <S.Post>
 
                     <S.PostHeader>
-                        <input type="text" minLength="1" maxLength="50" placeholder="제목 1~50자" required/>
+                        <S.PostTitle type="text" minLength="1" maxLength="50" placeholder="제목 1~50자" required/>
 
-                        <input type="text" maxLength="5" />
+                        <S.PostNickName type="text" maxLength="5" />
 
-                        <div>
+                        <S.PostTag>
                             tag
-                        </div>
+                        </S.PostTag>
                     </S.PostHeader>
 
                     
 
                     <S.PostContents 
                         cols="50"
-                        rows="12"
+                        rows="11"
                         onChange={onContent}
                         maxLength="500"
                         required
                     />
                     {writeContents.length} / 500
 
+                    <S.PostFile 
+                        type="file"
+                    />
+
                     <S.PostButtonBox>
+
                         <S.PostButton>완료</S.PostButton>
                         <S.PostButton onClick={onClose}>취소</S.PostButton>
                         

857397013d7560bff587c1e24f3505d949e30e89
oajh04 <cloudtubekr@naver.com>
2021-02-08 01:39:21 +0900

    comment

--- src/User/Components/Modal/CommentModal.js
+++ src/User/Components/Modal/CommentModal.js
@@ -1,14 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
+import CommentContainer from '../Container/CommentContainer';
 import * as S from '../styled/CommentStyle';
 
 const CommentModal = (props) => {
+    const [ commentNickName, setCommentNickName ] = useState("");
+    const [ commentContent, setCommentContent ] = useState("");
+
+    const onClose = () => {
+        props.setCommentModal("hidden");
+        props.setCommentHeight("0");
+    }
+
+    const onNickName = (e) => {
+        setCommentNickName(e.target.value)
+    }
+
+    const onContent = (e) => {
+        setCommentContent(e.target.value)
+    }
+
+    const onSend = () => {
+
+    }
 
     return (
         <>
-            <S.CommentModalStyle visibility={props.vi}>
-                <S.CommentModal>
+            <S.CommentModalStyle visibility={props.commentModal}>
+                <S.CommentModal height={props.commentheight}>
                     <S.Comment>
-                        sadfje
+                        <S.ContainerBox>
+                            <CommentContainer/>
+                            <CommentContainer/>
+                            <CommentContainer/>
+                            <CommentContainer/>
+                            <CommentContainer/>
+                            <CommentContainer/>
+                            <CommentContainer/>
+                            <CommentContainer/>
+                            <CommentContainer/>
+                            <CommentContainer/>
+                            <CommentContainer/>
+                        </S.ContainerBox>
+
+                        <S.CommentWrite>
+                            <S.WriteBox onSubmit={onSend}>
+                                <S.WriteInputBox>
+                                    <S.NickNameInput 
+                                        placeholder="별명"
+                                        maxLength="5"
+                                        onChange={onNickName}
+                                    />
+                                    <S.ContentInput 
+                                        placeholder="댓글을 입력하세요"
+                                        maxLength="100"
+                                        onChange={onContent}
+                                        required
+                                    />
+                                    {commentContent.length} / 100
+                                </S.WriteInputBox>
+
+                                <S.WriteButtonBox>
+                                    <S.WriteButton 
+                                        type="button"
+                                        onClick={onClose}
+                                    >
+                                        닫기
+                                    </S.WriteButton>
+                                    <S.WriteButton>
+                                        댓글달기
+                                    </S.WriteButton>
+                                </S.WriteButtonBox>
+                            </S.WriteBox>
+                        </S.CommentWrite>
                     </S.Comment>
                 </S.CommentModal>
             </S.CommentModalStyle>

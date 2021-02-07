afb49fa89324bc3845dfe8a71bd275d38e9b8f76
oajh04 <cloudtubekr@naver.com>
2021-02-08 01:39:09 +0900

    comment

---
+++ src/User/Components/Container/CommentContainer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import * as S from '../styled/CommentStyle';
+
+const Comments = () => {
+
+    return (
+        <>
+            <S.CommentContainer>
+                <S.CommentHeader>
+                    ㅇㅇㅇㅇㅇ | 21.02.06
+                </S.CommentHeader>
+                
+                <S.CommentBody>
+                    ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ
+                    
+                </S.CommentBody>
+                
+            </S.CommentContainer>
+        </>
+    )
+}
+
+export default Comments;

efdb582ec299c28404b0b1ee2a4d59bd5b0b3b2e
oajh04 <cloudtubekr@naver.com>
2021-02-08 22:36:36 +0900

    admin

---
+++ src/Admin/index.js
@@ -0,0 +1,4 @@
+export { default as Approval } from "./Components/Approval/Approval";
+export { default as Login } from "./Components/Login/Login";
+export { default as RemovePost } from "./Components/RemovePost/RemovePost";
+export { default as Report } from "./Components/Report/Report";

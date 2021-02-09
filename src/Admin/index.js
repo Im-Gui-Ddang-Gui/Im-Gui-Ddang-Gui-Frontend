c3a201cf15b64dc91aac1b78f9d9fd940a727736
oajh04 <cloudtubekr@naver.com>
2021-02-09 21:22:29 +0900

    admin

--- src/Admin/index.js
+++ src/Admin/index.js
@@ -2,3 +2,4 @@ export { default as Approval } from "./Components/Approval/Approval";
 export { default as Login } from "./Components/Login/Login";
 export { default as RemovePost } from "./Components/RemovePost/RemovePost";
 export { default as Report } from "./Components/Report/Report";
+export { default as Header } from "./Components/Header/Header";

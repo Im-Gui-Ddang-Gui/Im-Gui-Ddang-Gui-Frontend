ba49e99131b032ca0286957eb8c842905bd48704
oajh04 <cloudtubekr@naver.com>
2021-02-09 21:22:34 +0900

    app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Route, BrowserRouter, Switch } from "react-router-dom";
+import { Route, BrowserRouter } from "react-router-dom";
 import UserRouter from './Router/UserRouter'
+import AdminRouter from './Router/AdminRouter'
 
 const App = () => {
   return(
     <>
       <BrowserRouter>
         <Route path="/" component={UserRouter} />
+        <Route path="/" component={AdminRouter} />
       </BrowserRouter>
     </>
   )

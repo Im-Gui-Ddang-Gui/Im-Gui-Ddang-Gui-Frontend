55732401e66af6fc57b4ce796c9605ad1c930ab1
oajh04 <cloudtubekr@naver.com>
2021-02-09 21:22:09 +0900

    admin

--- src/Admin/Components/Approval/Approval.js
+++ src/Admin/Components/Approval/Approval.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { request } from '../../../utils/axios/axios'
 
 const Approval = () => {
+    useEffect(()=>{
+        const Refresh = async () => {
+            const TokenData = {
+                "X-Refresh-Token": localStorage.getItem("refresh-token")
+            }
+            try{
+                const response = await request(
+                    "put",
+                    "/admin/auth",
+                    {},
+                    TokenData
+                )
+            }
+            catch(e){
+                
+            }
+        }
+    })
 
     return (
         <>
-
+            approval
         </>
     )
 }

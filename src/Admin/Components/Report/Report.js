fcb7db55cad321e2441ac5145a9d7c100f351fd0
oajh04 <cloudtubekr@naver.com>
2021-02-10 21:48:51 +0900

    Report

--- src/Admin/Components/Report/Report.js
+++ src/Admin/Components/Report/Report.js
@@ -2,6 +2,24 @@ import React from 'react';
 import Header from '../Header/Header';
 
 const Report = () => {
+    useEffect(()=>{
+        const Refresh = async () => {
+            const TokenData = {
+                "X-Refresh-Token": localStorage.getItem("refresh-token")
+            }
+            try{
+                const response = await request(
+                    "put",
+                    "/admin/auth",
+                    {},
+                    TokenData
+                )
+            }
+            catch(e){
+                
+            }
+        }
+    })
 
     return (
         <>

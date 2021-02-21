d75a1161098b2bcc79c8f16ef9a70d64e96888c4
oajh04 <cloudtubekr@naver.com>
2021-02-22 00:02:37 +0900

    admin

---
+++ src/Admin/Components/RemovePost/PostContainer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import * as S from '../style/RemovePostStyle';
+
+const PostContainer = () => {
+
+    return (
+        <>
+            <S.PostContainer>
+                <S.PostHeader>
+                    <S.PostTitle>제목</S.PostTitle>
+                    <S.PostTag>태그</S.PostTag>
+                    <S.PostDate>21.22.22</S.PostDate>
+                    <S.PostRemove>삭제</S.PostRemove>
+                </S.PostHeader>
+                <S.PostContent>내용</S.PostContent>
+            </S.PostContainer>
+        </>
+    )
+}
+
+export default PostContainer;

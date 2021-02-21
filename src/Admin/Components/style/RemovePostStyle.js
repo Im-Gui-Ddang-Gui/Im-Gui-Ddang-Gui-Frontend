815c411bc454975cad9531e8be66367f5c22952a
oajh04 <cloudtubekr@naver.com>
2021-02-22 00:02:42 +0900

    admin
    admin

--- src/Admin/Components/style/RemovePostStyle.js
+++ src/Admin/Components/style/RemovePostStyle.js
@@ -1,1 +1,53 @@
 import styled from 'styled-components';
+
+export const RemovePost = styled.div`
+    width: 700px;
+    margin: 0 auto;
+    margin-top: 30px;
+    border: 1px solid black;
+    border-radius: 20px;
+    padding: 30px;
+`
+
+export const PostContainer = styled.div`
+    width: 80%;
+`
+
+export const PostHeader = styled.div`
+    margin: 0 auto;
+    width: 700px;
+    display: flex;
+    border-radius: 20px;
+    border: 1px solid black;
+`
+
+export const PostTitle = styled.div`
+    width: 65%;
+    padding: 15px;
+    padding-left: 20px;
+`
+
+export const PostTag = styled.div`
+    width: 6%;
+    padding: 15px;
+`
+
+export const PostDate = styled.div`
+    width: 10%;
+    padding: 15px;
+`
+
+export const PostRemove = styled.div`
+    width: 6%;
+    padding: 15px;
+`
+
+export const PostContent = styled.div`
+    width: 660px;
+    height: 300px;
+    padding: 20px;
+    border-radius: 20px;
+    border: 1px solid black;
+    margin: 0 auto;
+    margin-top: 10px;
+`

824a334ffea9e85f9c025758ba1787c7ed403046
oajh04 <cloudtubekr@naver.com>
2021-03-25 20:01:32 +0900

    typescript change

---
+++ src/User/Components/styled/HeaderStyle.tsx
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+
+export const HeaderStyle = styled.div`
+    display: flex;
+`
+
+export const HeaderName = styled.div`
+    padding: 30px;
+    padding-right: 245px;
+`
+
+export const Title = styled.div`
+    display: flex;
+    align-items: flex-end;
+
+    p{
+        margin: 0;
+        font-size: 30px;
+    }
+`
+
+export const Write = styled.div`
+    cursor: pointer;
+    width: 55px;
+    margin-top: 30px;
+    font-size: 20px;
+`
+
+export const HeaderLogo = styled.div`
+    display: block;
+    height: 150px;
+
+    img {
+        
+        width: 150px;
+    }
+`

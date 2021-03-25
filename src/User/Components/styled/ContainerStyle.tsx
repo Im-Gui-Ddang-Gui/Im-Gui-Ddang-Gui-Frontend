590db89733624a97ff942ea4a1392cd49d049fe0
oajh04 <cloudtubekr@naver.com>
2021-03-25 20:01:30 +0900

    typescript change

---
+++ src/User/Components/styled/ContainerStyle.tsx
@@ -0,0 +1,79 @@
+import styled from 'styled-components';
+
+export const ContainerStyle = styled.div`
+    background-color: white;
+    width: 100%;
+    height: 550px;
+`;
+
+export const ContainerHeader = styled.div`
+    padding-top: 20px;
+    margin: 20px;
+    display: flex;
+`
+
+export const HeaderId = styled.div`
+    width: 10%;
+    padding: 5px;
+    border: 1px solid black;
+    border-right: none;
+    text-align: center;
+`
+
+export const HeaderTitle = styled.div`
+    width: 60%;
+    margin-right: 18px;
+    padding: 5px;
+    border: 1px solid black;
+`
+
+export const HeaderTag = styled.div`
+    padding: 5px;
+    width: 7%;
+    margin-right: 18px;
+    border: 1px solid black;
+    text-align: center;
+`
+
+export const HeaderDate = styled.div`
+    padding: 5px;
+    width: 12%;
+    border: 1px solid black;
+    text-align: center;
+`
+
+export const Contents = styled.div`
+    height: 385px;
+    margin: 20px;
+    overflow: auto;
+    border: 1px solid black;
+    padding: 10px;
+`
+
+export const ButtonBox = styled.div`
+    width: 90%;
+    margin: 0 auto;
+`
+
+export const Button = styled.button`
+    width: 200px;
+    background-color: #ECECEC;
+    height: 30px;
+    border: none;
+    border-radius: 10px;
+    text-align: center;
+    padding: 3px;
+    cursor: pointer;
+`
+
+export const RightButton = styled.button`
+    width: 200px;
+    margin-left: 149px;
+    background-color: #ECECEC;
+    height: 30px;
+    border: none;
+    border-radius: 10px;
+    text-align: center;
+    padding: 3px;
+    cursor: pointer;
+`

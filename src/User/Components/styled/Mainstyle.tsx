7461d25450ea4c23871b12387a56193671413f81
oajh04 <cloudtubekr@naver.com>
2021-03-25 20:01:34 +0900

    typescript change

---
+++ src/User/Components/styled/Mainstyle.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Mainstyle = styled.div`
+    width: 611px;
+    border-left: 1px solid black;
+    border-right: 1px solid black;
+    margin: 0 auto;
+`;

e2448db744b2f444e13f44598f73a9637c73502e
oajh04 <67260242+oajh04@users.noreply.github.com>
2021-06-18 09:01:46 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,25 +4,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-<!--         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> -->
         <svg width="450" height="130" viewBox="0 0 450 130" fill="none" xmlns="http://www.w3.org/2000/svg">
             <rect x="0.5" y="0.5" width="449" height="129" rx="4" fill="white" stroke="#E4E2E2"/>
         </svg>
-      </header>
-    </div>
   );
 }
 
